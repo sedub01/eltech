a11eb9487afd0885aacb0034898390be1225349a
Семен Александрович Дубенков <61518799+sedub01@users.noreply.github.com>
2021-06-18 19:33:39 +0300

    Delete coursework directory

--- coursework/src/main/java/team/IRoles.java
+++
@@ -1,9 +0,0 @@
-package team;
-
-public interface IRoles{
-    public String[] roles = {"Вратарь", "Нападающий", "Полузащитник", "Защитник"};
-    public int GOALKEEPER = 0;
-    public int STRIKER = 1;
-    public int HALFBACK = 2;
-    public int FULLBACK = 3;
-}
